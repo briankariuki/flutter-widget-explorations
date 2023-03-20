a9c4cdf42dfaea0562a7e709edd56fc01d566c0a
briankariuki <briankariukidev@gmail.com>
2023-03-20 22:16:59 +0300

    add radius linear interpolation bubbles in fringe

--- lib/apple_bubble_ui/apple_bubble_ui_page.dart
+++ lib/apple_bubble_ui/apple_bubble_ui_page.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -29,12 +30,14 @@ class _AppleBubbleUIPageState extends State<AppleBubbleUIPage> {
 
   List<List<Bubble>> chunks = [];
 
-  double bubbleRadius = 24.0;
+  double maxBubbleRadius = 24.0;
+  double minBubbleRadius = 8.0;
+
   int bubbleRowItemCount = 5;
 
   double yRadius = 0.0;
   double xRadius = 0.0;
-  double gutterWidth = 60.0;
+  double gutterWidth = 80.0;
   double cornerRadius = 24.0;
 
   @override
@@ -57,7 +60,7 @@ class _AppleBubbleUIPageState extends State<AppleBubbleUIPage> {
         xRadius = (size.width / 2.5) / 2;
       });
 
-      var itemsFit = (size.width / ((bubbleRadius * 2) + 8.0)).floor();
+      var itemsFit = (size.width / ((maxBubbleRadius * 2) + 8.0)).floor();
 
       itemsFit = itemsFit;
 
@@ -97,11 +100,13 @@ class _AppleBubbleUIPageState extends State<AppleBubbleUIPage> {
                 painter: BubblesPainter(
                   bubbleChunks: chunks,
                   dragOffset: dragOffset,
-                  bubbleRadius: bubbleRadius,
+                  maxBubbleRadius: maxBubbleRadius,
+                  minBubbleRadius: minBubbleRadius,
                   cornerRadius: cornerRadius,
                   xRadius: xRadius,
                   yRadius: yRadius,
                   gutterWidth: gutterWidth,
+                  debug: true,
                 ),
                 size: Size.infinite,
               ),
@@ -143,20 +148,24 @@ class _AppleBubbleUIPageState extends State<AppleBubbleUIPage> {
 class BubblesPainter extends CustomPainter {
   final List<List<Bubble>> bubbleChunks;
   final Offset dragOffset;
-  final double bubbleRadius;
+  final double maxBubbleRadius;
+  final double minBubbleRadius;
   final double xRadius;
   final double yRadius;
   final double cornerRadius;
   final double gutterWidth;
+  final bool debug;
 
   BubblesPainter({
     required this.bubbleChunks,
     required this.dragOffset,
-    required this.bubbleRadius,
+    required this.maxBubbleRadius,
+    required this.minBubbleRadius,
     required this.xRadius,
     required this.yRadius,
     required this.cornerRadius,
     required this.gutterWidth,
+    this.debug = false,
   });
 
   @override
@@ -207,23 +216,51 @@ class BubblesPainter extends CustomPainter {
       Radius.circular(cornerRadius),
     );
 
+    final Rect topMidZone = Rect.fromLTRB(
+      canvasCenter.dx - xRadius + cornerRadius,
+      canvasCenter.dy - yRadius - gutterWidth,
+      canvasCenter.dx + xRadius - cornerRadius,
+      canvasCenter.dy - yRadius,
+    );
+
+    final Rect bottomMidZone = Rect.fromLTRB(
+      canvasCenter.dx - xRadius + cornerRadius,
+      canvasCenter.dy + yRadius,
+      canvasCenter.dx + xRadius - cornerRadius,
+      canvasCenter.dy + yRadius + gutterWidth,
+    );
+
+    final Rect leftMidZone = Rect.fromLTRB(
+      canvasCenter.dx - xRadius - gutterWidth,
+      canvasCenter.dy - yRadius + cornerRadius,
+      canvasCenter.dx - xRadius,
+      canvasCenter.dy + yRadius - cornerRadius,
+    );
+
+    final Rect rightMidZone = Rect.fromLTRB(
+      canvasCenter.dx + xRadius,
+      canvasCenter.dy - yRadius + cornerRadius,
+      canvasCenter.dx + xRadius + gutterWidth,
+      canvasCenter.dy + yRadius - cornerRadius,
+    );
+
     for (int i = 0; i < bubbleChunks.length; i++) {
-      final drawOffset = i % 2 == 0 ? 0 : bubbleRadius;
+      final drawOffset = i % 2 == 0 ? 0 : maxBubbleRadius;
 
       final bubbles = bubbleChunks[i];
 
       for (int j = 0; j < bubbles.length; j++) {
         Bubble bubble = bubbles[j];
 
-        var xPosition = dragOffset.dx + drawOffset + (j * bubbleRadius * 2) + bubbleRadius + (j * 8.0);
-        var yPosition = dragOffset.dy + 24.0 + i * bubbleRadius * 2;
+        var xPosition = dragOffset.dx + drawOffset + (j * maxBubbleRadius * 2) + maxBubbleRadius + (j * 8.0);
+        var yPosition = dragOffset.dy + 24.0 + i * maxBubbleRadius * 2;
 
-        var startPosition = Offset(xPosition, yPosition);
+        var bubblePosition = Offset(xPosition, yPosition);
 
         canvas.drawCircle(
-          startPosition,
+          bubblePosition,
           getBubbleRadius(
-            startPosition,
+            bubblePosition,
             bubble,
             canvasCenter,
             ((i * bubbles.length) + j),
@@ -233,42 +270,64 @@ class BubblesPainter extends CustomPainter {
             cornerZone4: cornerZone4,
             centerZone: centerZone,
             fringeZone: fringeZone,
+            leftMidZone: leftMidZone,
+            rightMidZone: rightMidZone,
+            topMidZone: topMidZone,
+            bottomMidZone: bottomMidZone,
           ),
           Paint()..color = bubble.color,
         );
       }
     }
 
-    //Define boundary rects
+    if (debug) {
+      //Paint boundary rects
 
-    canvas.drawRect(
-      cornerZone1,
-      Paint()..color = Colors.red.withOpacity(0.5),
-    );
+      canvas.drawRect(
+        cornerZone1,
+        Paint()..color = Colors.red.withOpacity(0.5),
+      );
 
-    canvas.drawRect(
-      cornerZone2,
-      Paint()..color = Colors.red.withOpacity(0.5),
-    );
+      canvas.drawRect(
+        cornerZone2,
+        Paint()..color = Colors.red.withOpacity(0.5),
+      );
 
-    canvas.drawRect(
-      cornerZone3,
-      Paint()..color = Colors.red.withOpacity(0.5),
-    );
+      canvas.drawRect(
+        cornerZone3,
+        Paint()..color = Colors.red.withOpacity(0.5),
+      );
 
-    canvas.drawRect(
-      cornerZone4,
-      Paint()..color = Colors.red.withOpacity(0.5),
-    );
+      canvas.drawRect(
+        cornerZone4,
+        Paint()..color = Colors.red.withOpacity(0.5),
+      );
 
-    canvas.drawRRect(
-      centerZone,
-      Paint()..color = Colors.blue.withOpacity(0.5),
-    );
-    canvas.drawRRect(
-      fringeZone,
-      Paint()..color = Colors.yellow.withOpacity(0.5),
-    );
+      canvas.drawRRect(
+        centerZone,
+        Paint()..color = Colors.blue.withOpacity(0.5),
+      );
+      canvas.drawRRect(
+        fringeZone,
+        Paint()..color = Colors.yellow.withOpacity(0.5),
+      );
+      canvas.drawRect(
+        topMidZone,
+        Paint()..color = Colors.green.withOpacity(0.5),
+      );
+      canvas.drawRect(
+        bottomMidZone,
+        Paint()..color = Colors.green.withOpacity(0.5),
+      );
+      canvas.drawRect(
+        leftMidZone,
+        Paint()..color = Colors.green.withOpacity(0.5),
+      );
+      canvas.drawRect(
+        rightMidZone,
+        Paint()..color = Colors.green.withOpacity(0.5),
+      );
+    }
   }
 
   @override
@@ -277,7 +336,7 @@ class BubblesPainter extends CustomPainter {
   }
 
   double getBubbleRadius(
-    Offset startPosition,
+    Offset bubblePosition,
     Bubble bubble,
     Offset canvasCenter,
     int index, {
@@ -287,32 +346,36 @@ class BubblesPainter extends CustomPainter {
     required Rect cornerZone4,
     required RRect centerZone,
     required RRect fringeZone,
+    required Rect topMidZone,
+    required Rect bottomMidZone,
+    required Rect leftMidZone,
+    required Rect rightMidZone,
   }) {
     //Check if is in center region
-    bool isInCenterZone = centerZone.contains(startPosition);
+    bool isInCenterZone = centerZone.contains(bubblePosition);
 
     //Check if is in fringe region
-    bool isInFringeZone = fringeZone.contains(startPosition);
+    bool isInFringeZone = fringeZone.contains(bubblePosition);
 
-    // bool isInCornerZone1 = startPosition.dx.abs() < (canvasCenter.dx - xRadius + cornerRadius) && startPosition.dy < canvasCenter.dy - yRadius + cornerRadius;
+    // bool isInCornerZone1 = bubblePosition.dx.abs() < (canvasCenter.dx - xRadius + cornerRadius) && bubblePosition.dy < canvasCenter.dy - yRadius + cornerRadius;
 
-    // bool isInCornerZone2 = startPosition.dx.abs() > (canvasCenter.dx + xRadius - cornerRadius) && startPosition.dy < canvasCenter.dy - yRadius + cornerRadius;
+    // bool isInCornerZone2 = bubblePosition.dx.abs() > (canvasCenter.dx + xRadius - cornerRadius) && bubblePosition.dy < canvasCenter.dy - yRadius + cornerRadius;
 
-    // bool isInCornerZone3 = startPosition.dx.abs() < (canvasCenter.dx - xRadius + cornerRadius) && startPosition.dy > canvasCenter.dy + yRadius - cornerRadius;
+    // bool isInCornerZone3 = bubblePosition.dx.abs() < (canvasCenter.dx - xRadius + cornerRadius) && bubblePosition.dy > canvasCenter.dy + yRadius - cornerRadius;
 
-    // bool isInCornerZone4 = startPosition.dx.abs() > (canvasCenter.dx + xRadius - cornerRadius) && startPosition.dy > canvasCenter.dy + yRadius - cornerRadius;
+    // bool isInCornerZone4 = bubblePosition.dx.abs() > (canvasCenter.dx + xRadius - cornerRadius) && bubblePosition.dy > canvasCenter.dy + yRadius - cornerRadius;
 
-    bool isInCornerZone1 = cornerZone1.contains(startPosition);
+    bool isInCornerZone1 = cornerZone1.contains(bubblePosition);
 
-    bool isInCornerZone2 = cornerZone2.contains(startPosition);
+    bool isInCornerZone2 = cornerZone2.contains(bubblePosition);
 
-    bool isInCornerZone3 = cornerZone3.contains(startPosition);
+    bool isInCornerZone3 = cornerZone3.contains(bubblePosition);
 
-    bool isInCornerZone4 = cornerZone4.contains(startPosition);
+    bool isInCornerZone4 = cornerZone4.contains(bubblePosition);
 
     if (isInCornerZone1 || isInCornerZone2 || isInCornerZone3 || isInCornerZone4) {
       // final distanceToCorner = getDistanceToPoint(
-      //   startPosition,
+      //   bubblePosition,
       //   Offset(
       //     canvasCenter.dx - xRadius + cornerRadius,
       //     canvasCenter.dy - yRadius + cornerRadius,
@@ -324,21 +387,74 @@ class BubblesPainter extends CustomPainter {
       // }
 
       if (isInFringeZone) {
-        return 16.0;
+        final innerPointPosition = isInCornerZone1
+            ? Offset(cornerZone1.right, cornerZone1.bottom)
+            : isInCornerZone2
+                ? Offset(cornerZone2.left, cornerZone2.bottom)
+                : isInCornerZone3
+                    ? Offset(cornerZone3.right, cornerZone3.top)
+                    : Offset(cornerZone4.left, cornerZone4.top);
+
+        final maxEndPointPosition = isInCornerZone1
+            ? Offset(fringeZone.left, fringeZone.top)
+            : isInCornerZone2
+                ? Offset(fringeZone.right, fringeZone.top)
+                : isInCornerZone3
+                    ? Offset(fringeZone.left, fringeZone.bottom)
+                    : Offset(fringeZone.right, fringeZone.bottom);
+
+        final maxDistanceToCornerPoint = getDistanceToPoint(maxEndPointPosition, innerPointPosition);
+
+        final distanceToInnerCornerPoint = getDistanceToPoint(bubblePosition, innerPointPosition);
+
+        return lerpDouble(
+          maxBubbleRadius,
+          minBubbleRadius,
+          (distanceToInnerCornerPoint / maxDistanceToCornerPoint),
+        )!;
       }
 
-      return 12.0;
+      return minBubbleRadius;
     }
 
     if (isInCenterZone) {
-      return bubbleRadius;
+      return maxBubbleRadius;
     }
 
     if (isInFringeZone) {
-      return 16.0;
+      if (topMidZone.contains(bubblePosition)) {
+        return lerpDouble(
+          minBubbleRadius,
+          maxBubbleRadius,
+          ((bubblePosition.dy - topMidZone.top).abs() / (topMidZone.bottom - topMidZone.top)),
+        )!;
+      }
+      if (bottomMidZone.contains(bubblePosition)) {
+        return lerpDouble(
+          maxBubbleRadius,
+          minBubbleRadius,
+          ((bottomMidZone.top - bubblePosition.dy).abs() / (bottomMidZone.bottom - bottomMidZone.top)),
+        )!;
+      }
+      if (leftMidZone.contains(bubblePosition)) {
+        return lerpDouble(
+          minBubbleRadius,
+          maxBubbleRadius,
+          ((leftMidZone.left - bubblePosition.dx).abs() / (leftMidZone.right - leftMidZone.left)),
+        )!;
+      }
+
+      if (rightMidZone.contains(bubblePosition)) {
+        return lerpDouble(
+          maxBubbleRadius,
+          minBubbleRadius,
+          ((rightMidZone.left - bubblePosition.dx).abs() / (rightMidZone.right - rightMidZone.left)),
+        )!;
+      }
+      return minBubbleRadius;
     }
 
-    return 8.0;
+    return minBubbleRadius;
   }
 
   double getDistanceToPoint(Offset startPosition, Offset endPosition) {
